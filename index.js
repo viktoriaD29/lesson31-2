0371c7d05483fa9a61a4a2c251fa99812211a59b
Viktoria <doduchenko29@gmail.com>
2021-06-22 19:13:32 +0300

    add

--- index.js
+++ index.js
@@ -9,13 +9,10 @@ export const asyncCalculator = (number) => new Promise((resolve) => {
       const result = value * value;
       console.log(`Squared value: ${result}`)
       resolve(result)
-    }, 3000)
+    }, 500)
   }))
   .then(value => {
     const result = value * 2;
     console.log(`Doubled value: ${result}`)
     return result
   })
-
-asyncCalculator(5)
-  .then(value => console.log(value))
